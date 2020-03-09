24e415db4008ed4e075dc0be06307c25a02f1965
yurilee9696 <yurilee9696@gmail.com>
2020-03-09 22:00:42 +0900

    MOD React Redux - mapStateToProps

--- src/containers/DisplayNumber.jsx
+++ src/containers/DisplayNumber.jsx
@@ -1,9 +1,25 @@
 import { connect } from "react-redux";
 import DisplayNumber from "../components/DisplayNumber";
 
-export default connect()(DisplayNumber); // wrapping된 content가 return
+function mapReduxStateToReactProps(reduxState) {
+  /*
+  store의 subscribe를 통해 redux의 변화가 일어났을 때 setState가 실행되면서 render가 실행돠는 역할
+  아래와 같은 의미를 가짐
+  state = { number: store.getState().number }
 
-// connect를 실행하면 자동으로 아래와 같은 처리를 해줌
+  store.subscribe(() => {
+      this.setState({ number: store.getState().number });
+    })
+
+  number={this.state.number}
+  */
+  return {
+    number: reduxState.number
+  }
+}
+export default connect(mapReduxStateToReactProps)(DisplayNumber); // wrapping된 content가 return
+
+// connect를 실행하면 자동으로 아래와 같은 container를 만들어줌
 /*
 export default class extends Component {
   state = { number: store.getState().number }
